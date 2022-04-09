60baa69cab83a6ba65941724199731c49c78d858
Marco Santonastasi <84954908+marco-ih-bcn@users.noreply.github.com>
2022-04-09 14:42:35 +0200

    clean main class

--- monopoly_game_part-2.js
+++ monopoly_game_part-2.js
@@ -1,52 +1,12 @@
 // Monopoly Game Part 2
+
 // Example of a VERY simple Monopoly game simulation
 
-const squares = [
-  100, -10, 0, 0, -40, -10, -10, 5, 0, -10, -50, -10, 0, 0, -50, -10,
-];
+// --- First, set up the board
 
-// ----------------------  Step 1  -----------------------
-// Creation of the class
-class Player {
-  // The constructor is the method triggered with the `new` keyword
-  constructor() {
-  }
-
-  // Method move
-  move() {
-  }
-
-  // Method displayInfo
-  displayInfo() {}
-}
-
-// --- Initialization of players ---
-var player1 =
-var player2 = 
-var player3 = 
+// --- Then initialize the game with a class ---
 
 // --- Turn 1  ---
-player1.move();
-player2.move();
-player3.move();
 
 // --- Turn 2  ---
-player1.move();
-player2.move();
-player3.move();
 
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-
-// ---------------------  Step 2  --------------------------
-
-class Game {
-  constructor() {
-    // --- Initialization of players ---
-
-    // ---- Init of game variables
-  }
-
-  play() {}
-}
